06c17c6e3c12dfeaed0e74bfb5efbc09f6bcd267
Karol Wasemann <kw.kwaq@gmail.com>
2022-07-04 15:50:34 +0200

    log

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ import { createServer } from "http";
 
 const port = process.env.PORT || 8080;
 const app = express();
-app.set("https://meet-up-dogs.netlify.app", 1);
+// app.set("https://meet-up-dogs.netlify.app", 1);
+// console.log("as");
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://meet-up-dogs.netlify.app",
-    // origin: "http://localhost:3000",
+    // origin: "https://meet-up-dogs.netlify.app",
+    origin: "http://localhost:3000",
 
     credentials: true,
   })
@@ -25,8 +26,8 @@ app.use(
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://meet-up-dogs.netlify.app",
-    // origin: "http://localhost:3000",
+    // origin: "https://meet-up-dogs.netlify.app",
+    origin: "http://localhost:3000",
 
     methods: ["GET", "POST"],
   },
