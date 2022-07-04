2a8f4f6d31ab246206d8139526cbe0be345ca810
Karol Wasemann <kw.kwaq@gmail.com>
2022-07-04 13:37:36 +0200

    cors

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -90,81 +90,81 @@ export const postLogin = async (req, res) => {
 };
 
 /** @param {express.Response} res */
-// export const postLogout = async (req, res) => {
-//   // Hatte der Nutzer ein Refresh Token Cookie?
-//   // const refreshToken = req.cookies?.refreshToken;
-//   // if (!refreshToken) return res.sendStatus(204); // Wenn cookie nicht da ist, kann man hier auch nicht mehr tun
-//   // console.log(req);
-//   // Lösche Cookies beim Client
-//   res.clearCookie("token", {
-//     httpOnly: false,
-//     sameSite: "none",
-//     secure: true,
-//   });
+export const postLogout = async (req, res) => {
+  // Hatte der Nutzer ein Refresh Token Cookie?
+  // const refreshToken = req.cookies?.refreshToken;
+  // if (!refreshToken) return res.sendStatus(204); // Wenn cookie nicht da ist, kann man hier auch nicht mehr tun
+  // console.log(req);
+  // Lösche Cookies beim Client
+  res.clearCookie("token", {
+    httpOnly: false,
+    sameSite: "none",
+    secure: true,
+  });
 
-//   // Lösche Refresh Token aus Datenbank
-//   // try {
-//   //   const databaseResponse = await UserModel.updateOne({refreshToken}, {refreshToken:''})
-//   //   // Refresh Token nicht gefunden?
-//   //   if(databaseResponse.matchedCount === 0) return res.sendStatus(204);
-//   // } catch (error) {
-//   //   console.error({error});
-//   //   return res.status(500).json({ msg: "logged out, but couldn't delete refresh Token from DB", error})
-//   // }
+  // Lösche Refresh Token aus Datenbank
+  // try {
+  //   const databaseResponse = await UserModel.updateOne({refreshToken}, {refreshToken:''})
+  //   // Refresh Token nicht gefunden?
+  //   if(databaseResponse.matchedCount === 0) return res.sendStatus(204);
+  // } catch (error) {
+  //   console.error({error});
+  //   return res.status(500).json({ msg: "logged out, but couldn't delete refresh Token from DB", error})
+  // }
 
-//   // Alles ok: Cookies und erfolgreich aus Datenbank gelöscht
-//   return res.status(200).send({ msg: "successfully logged out" });
-// };
+  // Alles ok: Cookies und erfolgreich aus Datenbank gelöscht
+  return res.status(200).send({ msg: "successfully logged out" });
+};
 
-// /** @param {express.Response} res */
-// export const postRefreshToken = async (req, res) => {
-//   // refreshToken cookie vorhanden?
-//   // kein refresh token cookie => kein neues access token
-//   const refreshToken = req.cookies.refreshToken;
-//   if (!refreshToken)
-//     return res.status(401).send({ error: "No refresh token cookie" }); // Unauthorized (keine Anmeldedaten vorhanden)
+/** @param {express.Response} res */
+export const postRefreshToken = async (req, res) => {
+  // refreshToken cookie vorhanden?
+  // kein refresh token cookie => kein neues access token
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken)
+    return res.status(401).send({ error: "No refresh token cookie" }); // Unauthorized (keine Anmeldedaten vorhanden)
 
-//   // Refresh Token noch in Datenbank?
-//   // Nein? => "abgelaufen" => kein neues access-token
-//   let loggedUser;
-//   try {
-//     loggedUser = await UserModel.findOne({ refreshToken });
-//     if (!loggedUser)
-//       return res.status(403).send({ error: "refresh token not found" }); //Forbidden  (Anmeldedaten vorhanden, aber nicht gültig)
-//     console.debug({ loggedUser });
-//   } catch (error) {
-//     console.error(error);
-//     return res.json({ error: error.message });
-//   }
+  // Refresh Token noch in Datenbank?
+  // Nein? => "abgelaufen" => kein neues access-token
+  let loggedUser;
+  try {
+    loggedUser = await UserModel.findOne({ refreshToken });
+    if (!loggedUser)
+      return res.status(403).send({ error: "refresh token not found" }); //Forbidden  (Anmeldedaten vorhanden, aber nicht gültig)
+    console.debug({ loggedUser });
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: error.message });
+  }
 
-//   // Refresh Token verifizieren (vorhanden in Datenbank, aber z.b. abgelaufen?)
-//   try {
-//     const decodedJwt = jwt.verify(
-//       refreshToken,
-//       process.env.REFRESH_TOKEN_SECRET
-//     );
-//     // zuätzliche Überprüfung:
-//     // Befindet sich im Token die gleiche _id wie in der Datebnak beim Nutzer, der das Refresh Token hat?
-//     if (loggedUser._id.toString() !== decodedJwt.userId)
-//       return res.sendStatus(403);
-//   } catch (error) {
-//     console.error("Refresh Token verify error", error);
-//     return res.sendStatus(403);
-//   }
+  // Refresh Token verifizieren (vorhanden in Datenbank, aber z.b. abgelaufen?)
+  try {
+    const decodedJwt = jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    // zuätzliche Überprüfung:
+    // Befindet sich im Token die gleiche _id wie in der Datebnak beim Nutzer, der das Refresh Token hat?
+    if (loggedUser._id.toString() !== decodedJwt.userId)
+      return res.sendStatus(403);
+  } catch (error) {
+    console.error("Refresh Token verify error", error);
+    return res.sendStatus(403);
+  }
 
-//   // Erstelle das neue Access-Token
-//   const accessToken = jwt.sign(
-//     {
-//       userName: loggedUser.name,
-//       userId: loggedUser._id,
-//     },
-//     process.env.TOKEN_SECRET,
-//     { expiresIn: EXPIRATION_ACCESTOKEN }
-//   );
+  // Erstelle das neue Access-Token
+  const accessToken = jwt.sign(
+    {
+      userName: loggedUser.name,
+      userId: loggedUser._id,
+    },
+    process.env.TOKEN_SECRET,
+    { expiresIn: EXPIRATION_ACCESTOKEN }
+  );
 
-//   return res.status(200).json({
-//     msg: "successfully refreshed token",
-//     accessToken,
-//     userName: loggedUser.name,
-//   });
-// };
+  return res.status(200).json({
+    msg: "successfully refreshed token",
+    accessToken,
+    userName: loggedUser.name,
+  });
+};
