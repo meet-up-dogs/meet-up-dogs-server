06a1ce77b6a3d2179310bf6d236a4a39dfaf8e7e
EveKlieber <Kliebereveline@gmail.com>
2022-07-12 18:41:02 +0200

    nodemailer cleanUp

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -2,16 +2,15 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = async (data) => {
 
-
   try {
-    
+
     // const mailOptions = {
     //   from: process.env.SMTP_MAIL,
     //   to: emailUser.email,
     //   subject: emailUser.subject,
     //   text: emailUser.message,
     // };
-    
+
     const options = {
       port: 465,  // 465 ist ein verschlüsselter port.
       host: 'mail.riseup.net',  // mailserver
@@ -21,28 +20,24 @@ const sendEmail = async (data) => {
         pass: process.env.SMTP_PASSWORD,
       },
       // tls: {
-        //   // do not fail on invalid certs
-        //   rejectUnauthorized: false,
-        // },
-      }
+      //   // do not fail on invalid certs
+      //   rejectUnauthorized: false,
+      // },
+    }
 
-      let transporter = nodemailer.createTransport(options)
-
+    let transporter = nodemailer.createTransport(options)
 
     let info = await transporter.sendMail({
       from: '<nodemailer@riseup.net>', // sender address
       to: "kliebereveline@gmail.com", // list of receivers
       subject: "neue nachricht von meet-up-dogs-frontend", // Subject line
       text: `${data.message} from d${data.email}`// plain text body
-      
+
       // html: "<b>Hello world?</b>", // html body
     });
 
     console.log("Message sent: %s", info.messageId);
-    console.log('try works')
-    console.log(data)
   }
-
 
   catch (error) {
     console.log(error)
