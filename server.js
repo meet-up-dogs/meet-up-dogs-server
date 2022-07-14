ab9805441b4d025366eee5cdea86d2580a9d2702
Karol Wasemann <kw.kwaq@gmail.com>
2022-07-14 14:24:58 +0200

    heroku

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ import chatModel from "./models/chat-model.js";
 
 const port = process.env.PORT || 8080;
 const app = express();
-// app.set("https://meet-up-dogs.netlify.app", 1);
+app.set("https://meet-up-dogs.netlify.app", 1);
 // console.log("as");
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "https://meet-up-dogs.netlify.app",
-    origin: "http://localhost:3000",
+    origin: "https://meet-up-dogs.netlify.app",
+    // origin: "http://localhost:3000",
 
     credentials: true,
   })
@@ -28,8 +28,8 @@ app.use(
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    // origin: "https://meet-up-dogs.netlify.app",
-    origin: "http://localhost:3000",
+    origin: "https://meet-up-dogs.netlify.app",
+    // origin: "http://localhost:3000",
 
     methods: ["GET", "POST"],
   },
