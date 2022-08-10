6703b1f36df525fb45a945737be3b4fee6fac071
EveKlieber <Kliebereveline@gmail.com>
2022-08-10 14:50:13 +0200

    deploy heroku

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ import chatModel from "./models/chat-model.js";
 //DEV
 const port = process.env.PORT || 8080;
 const app = express();
-app.set("https://meet-up-dog.netlify.app", 1);
+app.set("https://meet-up-dogs.netlify.app", 1);
 // console.log("as");
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://meet-up-dog.netlify.app",
+    origin: "https://meet-up-dogs.netlify.app",
     // origin: "http://localhost:3000",
 
     credentials: true,
@@ -28,7 +28,7 @@ app.use(
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://meet-up-dog.netlify.app",
+    origin: "https://meet-up-dogs.netlify.app",
     // origin: "http://localhost:3000",
 
     methods: ["GET", "POST"],
