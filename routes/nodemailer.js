d09a4b80f5bd5ecf9583afd0db1ce9ecfb9f1909
Karol Wasemann <kw.kwaq@gmail.com>
2022-08-16 09:16:53 +0200

    nodemailer

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -1,36 +1,31 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 const sendEmail = async (data) => {
-
   try {
-
     const options = {
-      port: 465,  // 465 ist ein verschlüsselter port.
-      host: 'mail.riseup.net',  // mailserver
+      port: 465, // 465 ist ein verschlüsselter port.
+      host: "mail.riseup.net", // mailserver
       secure: true, // meisten email anbieter verlangen hier verschlüsselung.
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASSWORD,
       },
-    }
+    };
 
-    let transporter = nodemailer.createTransport(options)
+    let transporter = nodemailer.createTransport(options);
 
     let info = await transporter.sendMail({
-      from: '<nodemailer@riseup.net>', // sender address
+      from: "<nodemailer@riseup.net>", // sender address
       to: "meetupdogs@gmail.com", // list of receivers
       subject: "neue nachricht von meet-up-dogs-frontend", // Subject line
-      text: `${data.message} from d${data.email}`// plain text body
+      text: `${data.message} from ${data.email}`, // plain text body
     });
 
     console.log("Message sent: %s", info.messageId);
-    console.log(data)
+    console.log(data);
+  } catch (error) {
+    console.log(error);
   }
-
-  catch (error) {
-    console.log(error)
-
-  }
-}
+};
 
 export default sendEmail;
